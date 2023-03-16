1dcf025b6efc0133ba8b38229d7bc8b8e9000dd0
Ubuntu <ubuntu@ip-172-31-63-199.ec2.internal>
2023-03-16 02:20:29 +0000

    Updated prediction file for server

--- prediction.py
+++ prediction.py
@@ -7,12 +7,6 @@ Created on Mon Dec  4 17:45:40 2017
 
 import pickle
 
-#doc_new = ['obama is running for president in 2016']
-
-var = input("Please enter the news text you want to verify: ")
-print("You entered: " + str(var))
-
-
 #function to run for prediction
 def detecting_fake_news(var):    
 #retrieving the best model for prediction call
@@ -20,9 +14,10 @@ def detecting_fake_news(var):
     prediction = load_model.predict([var])
     prob = load_model.predict_proba([var])
 
-    return (print("The given statement is ",prediction[0]),
-        print("The truth probability score is ",prob[0][1]))
+    return prediction[0], prob[0][1]
 
 
 if __name__ == '__main__':
-    detecting_fake_news(var)
+    var = input()
+    prediction, probability = detecting_fake_news(var)
+    print('echo {}'.format(probability))
